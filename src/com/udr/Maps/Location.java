c4f8403ba93f919cc086b47d48b0522bf2475a08
udr013 <udr013@gmail.com>
2016-04-04 13:56:34 +0200

    first commit

---
+++ src/com/udr/Maps/Location.java
@@ -0,0 +1,42 @@
+package com.udr.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by udr013 on 1-4-2016.
+ */
+public class Location {
+    private final int loctionID;
+    private final String description;
+    private final Map<String, Integer>exits;
+
+    public Location(int loctionID, String description,Map<String,Integer> exits) {
+        this.loctionID = loctionID;
+        this.description = description;
+        if(exits!=null){
+            this.exits = new HashMap<String, Integer>(exits);
+
+        }else{
+            this.exits= new HashMap<>();
+        }
+        exits.put("Q",0);
+
+    }
+
+//    public void addExit(String direction, int location){
+//        exits.put(direction, location);
+//    }
+
+    public int getLoctionID() {
+        return loctionID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return  new HashMap<String, Integer>(exits);
+    }
+}
